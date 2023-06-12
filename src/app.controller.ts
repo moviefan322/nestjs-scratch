f86fdcb685058c3ecbe194cc97a430f3ddbfcdd1
moviefan322 <philman202@yahoo.com>
2023-06-12 18:31:05 -0400

    adds goodbye world route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from "@nestjs/common";
 
-@Controller()
+@Controller("/app")
 export class AppController {
-  @Get()
+  @Get("/asdf")
   getRootRoute() {
     return "Hello World!";
+  }
+
+  @Get("/bye")
+  getRootRoute2() {
+    return "Goodbye World!";
   }
 }
